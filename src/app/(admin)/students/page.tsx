12bd4e9a40245a2beb8cb714f880ad0557410241
Joel Hernandez <jhernandez98@outlook.es>
2025-07-08 09:18:22 -0400

    add search component ui

--- src/app/(admin)/students/page.tsx
+++ src/app/(admin)/students/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from 'react';
 import StudentMetrics from '@/components/Students/Students-Metrics/StudentMetrics';
+import Search from '@/components/ui/search/Search';
 import util from '../../../styles/utils.module.css';
+import style from './style.module.css';
 
 const page = () => {
   return (
@@ -8,6 +10,13 @@ const page = () => {
      <Suspense fallback="Loading...">
         <StudentMetrics/>
      </Suspense>
+
+     <section className={`${style.student_table_container} ${util.card_container}`}>
+       <header className={style.student_table_header}>
+         <Search placeholder='Search name, etc'/>
+         
+       </header> 
+     </section>
 
     </main>
   )
