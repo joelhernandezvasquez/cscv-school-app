34215f896d1c893c3c81a84501069d4ede0c1ad8
Joel Hernandez <jhernandez98@outlook.es>
2025-07-16 08:43:07 -0400

    fix active button

--- src/components/ui/action-button/ActionButton.tsx
+++ src/components/ui/action-button/ActionButton.tsx
@@ -3,8 +3,8 @@ import style from './style.module.css';
 const ActionButton = () => {
   return (
     <button className={style.action_btn}>
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+      <svg  fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
      </svg>
     </button>
   )
