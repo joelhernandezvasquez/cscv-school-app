15f221d33d39505e0eb9709dab6117f2bf97e26d
Joel Hernandez <jhernandez98@outlook.es>
2025-07-23 07:16:59 -0400

    add student data into mobile screen

--- src/components/Students/StudentTableItem/StudentTableItem.tsx
+++ src/components/Students/StudentTableItem/StudentTableItem.tsx
@@ -1,12 +1,21 @@
 import Avatar from '@/components/ui/avatar/Avatar';
-import style from './style.module.css';
+import Badge from '@/components/ui/badge/Badge';
+import ActionButton from '@/components/ui/action-button/ActionButton';
+import { Students } from '@/types';
 import {MdOutlinePhone } from 'react-icons/md';
 import { CiCalendar } from 'react-icons/ci';
 import { FaBookBible } from 'react-icons/fa6';
-import Badge from '@/components/ui/badge/Badge';
-import ActionButton from '@/components/ui/action-button/ActionButton';
+import { getFormattedDate } from '@/lib/utils';
+import style from './style.module.css';
+interface Props{
+  student:Students
+}
 
-const StudentTableItem = () => {
+const StudentTableItem = ({student}:Props) => {
+
+  const {first_name,last_name,phone,active,created_at} = student;
+  const enrollmentDate = getFormattedDate(created_at);
+
   return (
     <li className={style.student_item}>
       <div className={style.student_left_col}>
@@ -14,15 +23,15 @@ const StudentTableItem = () => {
         <div className={style.student_info}>
            <Avatar width={30} height={30}/>
            <div>
-             <span>Joel Hernandez</span>
+             <span>{first_name} {last_name}</span>
              <p className={style.student_info_text}> 
             <MdOutlinePhone size={20}/>
-            (646)-841-6837
+            {phone}
            </p>
 
            <p className={style.student_info_text}> 
             <CiCalendar size={20}/>  
-             Feb 5, 2024
+           {enrollmentDate}
            </p>
 
            </div>
@@ -36,8 +45,7 @@ const StudentTableItem = () => {
             <FaBookBible size={18} color="#5655D7" />
             10
            </p>
-
-             <Badge status="Active"/>
+             <Badge status= {active ? 'Active' : 'Inactive'}/>
           
       </div>
     </li>
