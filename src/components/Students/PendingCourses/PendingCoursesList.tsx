8eb4a56bcd9741a0aa103f2ab98496be13aa68b6
Joel Hernandez <jhernandez98@outlook.es>
2025-10-04 17:41:56 -0400

    update

--- src/components/Students/PendingCourses/PendingCoursesList.tsx
+++ src/components/Students/PendingCourses/PendingCoursesList.tsx
@@ -10,6 +10,7 @@ interface Props{
 
 const PendingCoursesList = async ({studentId}:Props) => {
   const pendingCourses = await getPendingCoursesList(studentId);
+  console.log(pendingCourses);
   const pendingCoursesFormatted = getCoursesFormatted(pendingCourses);
   
    return (
--- src/lib/actions/students/index.ts
+++ src/lib/actions/students/index.ts
@@ -229,7 +229,7 @@ export const validateStudentForm = (formData:FormData) =>{
             'Content-Type': 'application/json'
           }
      })
-
+     console.log(await pendingCoursesRequest.json());
      return await pendingCoursesRequest.json();
     }
     catch(error){
