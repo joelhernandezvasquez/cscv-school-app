5fa51e142094714e0e12fd2e8a28743aa9750f95
Joel Hernandez <jhernandez98@outlook.es>
2025-10-21 06:41:36 -0400

    add past courses UI

--- src/app/(admin)/students/[id]/manual-course/page.tsx
+++ src/app/(admin)/students/[id]/manual-course/page.tsx
@@ -1,13 +1,7 @@
-
 import ManualCourseTable from "@/components/Students/ManualCourseTable/ManualCourseTable";
 import { getPendingCoursesList } from "@/lib/actions/students";
 import { mappedPendingCourses } from "@/lib/utils";
 import util from '../../../../../styles/utils.module.css';
-
-/* 
- 6- make it responsive
- 10- move functionalities to a custom hook(here)
-*/
 interface Props {
   params: { id: string };
 }
