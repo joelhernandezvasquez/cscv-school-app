528a97f00c9fa7d6dfdaf258a8be8483eeb55099
Joel Hernandez <jhernandez98@outlook.es>
2025-10-31 16:30:11 -0400

    add filter tab

---
+++ src/components/Events/filter-event-tabs/FilterEventTabs.tsx
@@ -0,0 +1,67 @@
+
+'use client';
+
+import { useState } from 'react';
+import { ChevronDown } from 'lucide-react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { eventTabs } from '@/lib/constants';
+import style from './style.module.css';
+import Dropdown from '@/components/ui/dropdown/Dropdown';
+import UseToggle from '@/hooks/UseToggle';
+import UseClickAway from '@/hooks/UseClickAway';
+
+const FilterEventTabs = () => {
+
+   const [currentActiveTab,setCurrentActiveTab] = useState('All');
+   const {isToggle,handleToggle} = UseToggle();
+   const dropdownRef = UseClickAway(handleToggle);
+   const searchParams = useSearchParams();
+   const pathname = usePathname();
+   const {replace} = useRouter();
+
+   const onChangeTab = (value:string) =>{
+      setCurrentActiveTab(value);
+      const params = new URLSearchParams(searchParams);
+      params.set('page','1');
+
+    if(value){
+      params.set('query', value);
+    }
+    else{
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+    handleToggle();
+   }
+
+  return (
+    <section className={style.container}>
+
+     <button className={style.filter_mobile_btn} onClick={handleToggle}>
+      <span>{currentActiveTab}</span>
+      <ChevronDown size={16} />
+    </button>
+
+      <ul className={style.filter_container}>
+       {eventTabs.map((event)=>{
+        return <li key={event.id}>{
+             <button className={`${style.filter_tab_button} ${currentActiveTab===event.value && style.active_btn}`} onClick={() => onChangeTab(event.value)}>{event.value}</button>
+        }</li>
+       })}
+    </ul>
+
+     {isToggle &&
+        <div ref={dropdownRef}>
+          <Dropdown 
+          className={style.dropdown_style} 
+          items={eventTabs.map((item)=> item.value)}
+          onClose={onChangeTab}
+          />
+        </div>  
+     }
+    </section>
+   
+  )
+}
+
+export default FilterEventTabs
