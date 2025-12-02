9c6f8f6c9fc7f028b2caa77eeeaea9d5a8df94b1
Joel Hernandez <jhernandez98@outlook.es>
2025-12-02 09:49:50 -0500

    add header to event detail page

--- src/app/(admin)/events/[id]/page.tsx
+++ src/app/(admin)/events/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
 import { getEvent } from "@/lib/actions/events";
+import EditEventButton from "@/components/Events/edit-event-btn/EditEventButton";
+import util from '../../../../styles/utils.module.css';
 
 interface EventDetailPageProps {
   params: { id: string }
@@ -14,9 +16,14 @@ const EventDetailPage = async ({ params }: EventDetailPageProps) => {
     }
 
   return (
-    <div>
-        <h2>Event Detail page</h2>
-    </div>
+   <main className={util.wrapper}>
+    <section className={util.card_container}>
+      <header className={`${util.flex} ${util.flex_center_space_between}`}>
+        <h2 className='title'>{event.name}</h2>
+        <EditEventButton/>
+      </header>
+    </section>
+   </main>
   )
 }
 
