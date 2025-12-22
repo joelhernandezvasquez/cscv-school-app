04c62b18f51f691c09e27e0af70df1db7bd63de8
Joel Hernandez <jhernandez98@outlook.es>
2025-12-22 12:40:06 -0500

    add list student enroll ui

--- src/components/Events/enroll-event-form/EnrollEventForm.tsx
+++ src/components/Events/enroll-event-form/EnrollEventForm.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { ChangeEvent, startTransition, useState} from 'react';
+import { ChangeEvent,startTransition, useState} from 'react';
 import EventEnrollStudentList from '../event-enroll-list-student/EventEnrollStudentList';
 import {searchStudents} from '@/lib/actions/students';
 import { Students } from '@/types';
@@ -8,30 +8,31 @@ import style from './style.module.css';
 
 const EnrollEventForm = () => {
   const [query,setQuery] = useState('');
-  const [result,setResult] = useState<Students[]>();
+  const [result,setResult] = useState<Students[]>([]);
 
   const handleStudentSearch = (event:ChangeEvent<HTMLInputElement>) =>{
-       setQuery(event.target.value);
+       const value = (event.target as HTMLInputElement).value;
+       setQuery(value);
        
        startTransition(async()=>{
-          const result = await searchStudents(query);
+          const result = await searchStudents(value);
           setResult(result);
        })
     }
   
   return (
-    <form className={style.enrollment_form}>
+    <section className={style.enrollment_form}>
       <div className={style.enrollment_form_search}>
          <LuSearch size={25} />
-         <input type='text' name='search' id='search' placeholder='Search By Name'
+         <input type='text' name='search' id='search' value={query} placeholder='Search By Name'
           onChange={(event) => handleStudentSearch(event)}
          />
       </div>
-      
-      { result && result.length > 0 &&
-        <EventEnrollStudentList studentList={result}/>
+      {
+        query && <EventEnrollStudentList studentList={result}/>
       }
-    </form>
+        
+    </section>
   )
 }
 
--- src/components/Events/event-enroll-list-student/EventEnrollStudentList.tsx
+++ src/components/Events/event-enroll-list-student/EventEnrollStudentList.tsx
@@ -1,14 +1,44 @@
-
+import Avatar from '@/components/ui/avatar/Avatar';
+import EmptyStudentSearch from '../empty-student-search/EmptyStudentSearch';
 import { Students } from '@/types';
+import style from './style.module.css';
+import button from '../../../styles/buttons.module.css';
 
 interface Props{
     studentList:Students []
 }
 
-const EventEnrollStudentList = ({studentList}:Props) => {
-      
+const EventEnrollStudentList = ({studentList}:Props) => {    
   return (
-    <div>{JSON.stringify(studentList)}</div>
+    <>
+     {
+      studentList.length > 0 
+      ? (
+        <ul className={style.student_list_container}>
+      {studentList.map((student)=>{
+        return <li className={style.student_item} key={student.id}>
+                 <div className={style.student_item_info_header}>
+                     <Avatar width={40} height={40}/>
+                     <div>
+                         <h3>{student.first_name} {student.last_name}</h3>
+                          <p>
+                            <span>{student.email} </span>
+                            <span>{student.phone}</span>
+                          </p>
+                     </div>
+                    
+                 </div>
+
+                 <button className={button.add_button}>Register</button>
+              </li>
+      })}
+       </ul>
+      )
+      :
+      <EmptyStudentSearch/>
+     }
+    
+    </>
   )
 }
 
