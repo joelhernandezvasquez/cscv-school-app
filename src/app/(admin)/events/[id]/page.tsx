edaa8ef4305b5d20ff6e8c167349e301bc3dbe96
Joel Hernandez <jhernandez98@outlook.es>
2026-01-12 17:18:16 -0500

    add go back link

--- src/app/(admin)/events/[id]/page.tsx
+++ src/app/(admin)/events/[id]/page.tsx
@@ -4,6 +4,7 @@ import EditEventButton from "@/components/Events/edit-event-btn/EditEventButton"
 import EventDetailInfo from "@/components/Events/event-detail-info/EventDetailInfo";
 import EventEnrollmentTable from "@/components/Events/event-enrollment-table/EventEnrollmentTable";
 import UpdateEventForm from "@/components/Events/updateEventForm/UpdateEventForm";
+import GoBackLink from "@/components/ui/go-back-link/GoBackLink";
 import { getEnrollmentsPerEvent } from "@/lib/actions/enrollments";
 import style from '../style.module.css';
 import util from '../../../../styles/utils.module.css';
@@ -24,7 +25,11 @@ const EventDetailPage = async ({ params }: EventDetailPageProps) => {
    <main className={util.wrapper}>
     <section className={util.card_container}>
       <header className={`${util.flex} ${util.flex_center_space_between}`}>
-        <h2 className='title'>{event.name}</h2>
+        <div className={`${util.flex} ${util.flex_col} ${util.gap_5}`}>
+          <h2 className='title'>{event.name}</h2>
+          <GoBackLink/>
+        </div>
+        
         <EditEventButton>
            <UpdateEventForm event={event}/>
         </EditEventButton>
