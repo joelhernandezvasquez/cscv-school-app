78d6eb42af4590a9750c269ed567ecad5fd3a161
Joel Hernandez <jhernandez98@outlook.es>
2026-02-02 17:26:43 -0500

    sync

--- src/components/ui/pagination/PaginationContainer.tsx
+++ src/components/ui/pagination/PaginationContainer.tsx
@@ -15,7 +15,11 @@ const PaginationContainer = async ({currentPage,query,sortBy,pagination}:Props) 
   
   const {totalPages,totalCount} = pagination;
   const pages = Array.from({length:totalPages},(_,i)=> i + 1 );
+  console.log(currentPage)
+  console.log(query)
+  console.log(sortBy)
 
+  console.log(pagination);
 
   return (
     <div className={style.pagination_container}>
