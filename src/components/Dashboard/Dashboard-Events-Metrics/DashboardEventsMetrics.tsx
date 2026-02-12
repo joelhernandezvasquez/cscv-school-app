17f87db061dff5a82898cf4c410d48283ecf72ea
Joel Hernandez <jhernandez98@outlook.es>
2026-02-12 07:01:27 -0500

    Dashboard Attention Metric integrated

--- src/components/Dashboard/Dashboard-Events-Metrics/DashboardEventsMetrics.tsx
+++ src/components/Dashboard/Dashboard-Events-Metrics/DashboardEventsMetrics.tsx
@@ -1,11 +1,12 @@
+import { getDashboardEventsSummary } from '@/lib/actions/dashboard';
 import BlockMetric from '@/components/ui/block-metric/BlockMetric';
-import { LiaCalendar } from 'react-icons/lia';
-import { FaDisplay } from 'react-icons/fa6';
+import { MdOutlineAirplay } from 'react-icons/md';
 import { CiCalendar, CiWarning } from 'react-icons/ci';
 import style from './style.module.css';
-import { MdOutlineAirplay } from 'react-icons/md';
 
-const DashboardEventsMetrics = () => {
+const DashboardEventsMetrics = async () => {
+   const {upcomingEvents,activeEvents,pendingCompletionEvents} = await getDashboardEventsSummary();
+    
   return (
     <section className={style.dashboard_events_metric_container}>
       <p className="sub_title">Overview of Events</p>
@@ -15,8 +16,8 @@ const DashboardEventsMetrics = () => {
           <BlockMetric 
             icon={<CiCalendar size={35} color={'#2a4de5'}/>} 
             title={'Upcoming Events'} 
-            metric={5} 
-            infoText={'Next 7 Days'}
+            metric={upcomingEvents.amount} 
+            infoText={`Next ${upcomingEvents.nextEventInDays} Days`}
           />
         </li>
 
@@ -24,7 +25,7 @@ const DashboardEventsMetrics = () => {
           <BlockMetric 
             icon={<MdOutlineAirplay size={35} color={'#249a67'}/>} 
             title={'Active Events'} 
-            metric={3} 
+            metric={activeEvents.amount} 
             infoText={'Currently Running'}
           />
         </li>
@@ -33,14 +34,12 @@ const DashboardEventsMetrics = () => {
           <BlockMetric 
             icon={<CiWarning size={35} color={'#ecb00ac4'}/>} 
             title={'Pending Completions'} 
-            metric={4} 
+            metric={pendingCompletionEvents.amount} 
             infoText={'Needs Attention'}
           />
         </li>
       </ul>
 
-     
-     
     </section>
   )
 }
--- src/lib/actions/dashboard/index.ts
+++ src/lib/actions/dashboard/index.ts
@@ -23,3 +23,26 @@ export const getDashboardSummary = async() =>{
                 throw new Error('Unknown error occurred while getting the courses');
           }
 }
+
+export const getDashboardEventsSummary = async() =>{
+   try{
+        const token = await getValidatedToken();
+        const dashboardEventSummary = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/dashboard/events/summary`,{
+         method: 'GET',
+           headers: {
+             'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json'
+            }
+       })
+  
+       return await dashboardEventSummary.json();
+      }
+      catch(error){
+        if(error instanceof Error){
+                console.log(error);
+                throw new Error(error.message);
+            } 
+              console.log(error);
+                throw new Error('Unknown error occurred while getting the courses');
+          }
+}
