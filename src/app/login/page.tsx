e0d860ec0941379ae0c55717afa56ab0ce43a8eb
Joel Hernandez <jhernandez98@outlook.es>
2026-02-24 06:44:33 -0500

    improve login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,15 @@
 import { auth } from '@/auth.config';
 import { redirect } from 'next/navigation';
+import { DM_Serif_Display } from 'next/font/google';
 import Logo from '@/components/ui/logo/Logo';
 import LoginForm from '@/components/Login/LoginForm/LoginForm';
 import style from './style.module.css';
+
+const DMSerifDisplay = DM_Serif_Display({
+  weight: "400",
+  variable:"--font-dm-serif-display",
+  subsets:["latin"]
+});
 
 const LoginPage = async() => {
 
@@ -13,16 +20,23 @@ const LoginPage = async() => {
       }
   
   return (
-    <section className={style.wrapper}>
-     
-     <div className={style.logo}>
-       <Logo width={45} height={45}/>
-       <span className={style.academy_text}>CSCV Academy</span>
-     </div>
-     
-     <h1 className={style.main_headline}>Sign in to CSCV Academy</h1>
-     <LoginForm/>
-    </section>
+    <>
+      <section className={style.wrapper}>
+        <div className={style.logo}>
+          <Logo width={45} height={45}/>
+          <span className={style.academy_text}>CSCV Academy</span>
+        </div>
+      
+        <h1 className={`${style.main_headline} ${DMSerifDisplay.className}`}>
+          Welcome back.
+        </h1>
+        <LoginForm fontType={DMSerifDisplay.className}/>
+        
+        <p className={`${style.footer_note}`}>
+          By signing in you agree to our <a href="#">Terms</a> & <a href="#">Privacy Policy</a>
+         </p>
+      </section>
+    </>
   )
 }
 
--- src/components/Login/LoginForm/LoginForm.tsx
+++ src/components/Login/LoginForm/LoginForm.tsx
@@ -1,12 +1,20 @@
 'use client';
+import { useTransition } from 'react';
 import { authenticateUser, signInUser } from '@/lib/actions/auth/login';
 import { checkLoginFormErrors } from '@/lib/utils';
 import { toast, Toaster } from 'sonner';
+import Spinner from '@/components/ui/spinner/Spinner';
 import style from './style.module.css';
 import form from '../../../styles/forms.module.css';
 import button from '../../../styles/buttons.module.css';
 
-const LoginForm = () => {
+interface Props{
+  fontType:string
+}
+
+const LoginForm = ({fontType}:Props) => {
+  
+  const [isPending, startTransition] = useTransition();
 
   const handleAuthentication = async(event:React.FormEvent<HTMLFormElement>) =>{
     event.preventDefault();
@@ -20,7 +28,7 @@ const LoginForm = () => {
       toast.error(message);
       return;
     }
-
+    startTransition(async() =>{
     const result = await authenticateUser(email,password);
     
     if(result.error){
@@ -28,30 +36,37 @@ const LoginForm = () => {
       return;
     }
 
-   await signInUser(email,password);
+      await signInUser(email,password);
+    })
   }
 
   return (
     <form className={style.form_container} onSubmit={handleAuthentication}>
        <div className={form.form_field}>
-         <label htmlFor='email'>Email Address</label>
+         <label className={`${form.bold_label} ${fontType}`} htmlFor='email'>Email Address</label>
          <div className={form.input_wrapper}>
-          <span>@</span>
+        <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="4" width="20" height="16" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
           <input type='email' name='email' id="email" placeholder='Enter email to get started' autoComplete="username"/>
          </div>
          
        </div>
 
        <div className={form.form_field}>
-         <label htmlFor='password'>Password</label>
+         <label className={`${form.bold_label} ${fontType}`} htmlFor='password'>Password</label>
          <div className={form.input_wrapper}>
-          <svg width={18} height={18} fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"></path></svg>
+          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="11" width="18" height="11" rx="2"/><path d="M7 11V7a5 5 0 0 1 10 0v4"/></svg>
           <input type='password' name='password' id="password" placeholder="Enter your password" autoComplete='off'/>
          </div>
         
        </div>
          <Toaster position="top-center" richColors  closeButton  />
-       <button type='submit' className={`${button.primary_btn} ${button.login_btn}`}>Log in</button>
+        <button 
+          type='submit' 
+          className={`${button.primary_btn} ${button.btn_login} ${fontType}`}
+          >
+            {!isPending ? 'Log in' : <Spinner text='Signing in…'/> }
+             
+          </button>
     </form>
   )
 }
